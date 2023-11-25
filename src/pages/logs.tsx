5063549c1857195f9a2bf085d04af8c2955c6537
taikulawo <qaq1362211689@gmail.com>
2023-11-25 13:52:16 +0800

    1

--- src/pages/logs.tsx
+++ src/pages/logs.tsx
@@ -90,6 +90,7 @@ const LogPage = () => {
             sx={{ width: 120, mr: 1, '[role="button"]': { py: 0.65 } }}
           >
             <MenuItem value="all">ALL</MenuItem>
+            <MenuItem value="debug">DEBUG</MenuItem>
             <MenuItem value="inf">INFO</MenuItem>
             <MenuItem value="warn">WARN</MenuItem>
             <MenuItem value="err">ERROR</MenuItem>
